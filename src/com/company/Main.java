f9599d5f4b4ae6c435fbe53d56c50cd020c0a50d
fisent <lukzieniewicz@gmail.com>
2017-10-29 23:19:35 +0100

    back to start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,15 @@ package com.company;
 
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class Main {
 
+
+
+
     public static void main(String[] args) {
-        JFrame ramka = new JFrame();
-        ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ramka.setTitle("Hello graphics");
-
-        MyPanel panel = new MyPanel();
-
-        ramka.getContentPane().add(panel);
-        ramka.setSize(300, 300);
-        ramka.setVisible(true);
+        System.out.println("Hello graphics");
     }
 }
---
+++ src/com/company/View/ImagePanel.java
@@ -0,0 +1,9 @@
+package com.company.View;
+
+import javax.swing.*;
+
+/**
+ * Created by Fisent on 29.10.2017.
+ */
+public class ImagePanel extends JPanel {
+}
